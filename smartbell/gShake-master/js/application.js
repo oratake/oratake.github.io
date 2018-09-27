ef4fe746585542d75dbaceed0d5405a817ca4047
oratake <smith.shimomura@gmail.com>
2018-09-27 17:48:00 +0900

    [clean]ポートフォリオ以外のテストデータ削除

--- smartbell/gShake-master/js/application.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	
-	$(this).gShake(function() {
-		alert("function loaded from document")
-	});
-	
-});
--- texxxx/js/application.js
+++
@@ -1,9 +0,0 @@
-window.onload = () => {
-  let table = document.createElement("table");
-  let thead = document.createElement("thead");
-  let tbody = document.createElement("tbody");
-  let cells = [];
-  for(let i = 0; i < 21; ++i) {
-    
-  }
-}
